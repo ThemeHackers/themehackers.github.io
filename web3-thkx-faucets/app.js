0d5457e52720716f979bec898428f5058f782bfa
ds_zct <107047730+ThemeHackers@users.noreply.github.com>
2025-03-13 20:39:29 +0700

    Update app.js

--- web3-thkx-faucets/app.js
+++ web3-thkx-faucets/app.js
@@ -6,7 +6,7 @@ let provider;
 let accounts = [];
 let selectedAccount;
 let lastClaimTime = localStorage.getItem("lastClaimTime") || 0;
-const claimCooldown =  2 * 60 * 60;
+const claimCooldown =  60 * 60;
 let claimAttempts = parseInt(localStorage.getItem("claimAttempts")) || 0;
 const maxAttempts = 5;
 let countdownInterval;
