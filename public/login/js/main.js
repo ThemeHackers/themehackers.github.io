5abe57d4b96c63953b9e32fffea6c021debf33be
_tthemzdl5678 <107047730+ThemeHackers@users.noreply.github.com>
2025-07-15 15:16:52 +0700

    Update main.js

--- public/login/js/main.js
+++ public/login/js/main.js
@@ -9,7 +9,7 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
 console.log('SUPABASE_URL', SUPABASE_URL);
 console.log('SUPABASE_ANON_KEY', SUPABASE_ANON_KEY);
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const googleBtn = document.getElementById('google-signin');
 const alertBox = document.getElementById('login-alert');
