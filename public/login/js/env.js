2d77c26f2e0eea7d016c6d4bec540fcfa70c0fe3
_tthemzdl5678 <107047730+ThemeHackers@users.noreply.github.com>
2025-07-15 14:47:45 +0700

    Add files via upload

--- public/login/js/env.js
+++ public/login/js/env.js
@@ -1,3 +1,4 @@
+// WARNING: Do not put any secrets in this file. Only public/anon keys should be here.
 window.ENV = {
   SUPABASE_URL: "https://tcjxrlsebxdyoohcugsr.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjanhybHNlYnhkeW9vaGN1Z3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1NTc0MTIsImV4cCI6MjA2ODEzMzQxMn0.fyaTfMKZqudmXSc6UxwDksMs64EgMCEsKXy_M_Ypz2k"
